0cea5bcb276ff2f99ce696cf61f1410fe089ab87
Maksim Romanov <ios38ru@gmail.com>
2020-07-16 13:35:03 +0800

    # - init project

---
+++ GBShop/GBShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Maksim Romanov on 16.07.2020.
+//  Copyright © 2020 Maksim Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
